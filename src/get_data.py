33262e983bf08484c43ee21a08f6cb7476ea2e5f
Will Clark <wfclark5@outlook.com>
2021-09-17 13:41:27 -0400

    adding updating scripts with prefect flows

--- src/get_data.py
+++ src/get_data.py
@@ -1,42 +1,130 @@
+import sys
 from toolbox.scrapers import *
+from prefect import task, Flow
 
-hospital_urls = os.path.join(url_download_path, 'hospital_data_urls.json')
+# abspath = os.path.normpath(os.path.dirname(os.path.dirname(__file__)))
 
-urls_json = json.load(open(hospital_urls))
 
+@task
+def get_source_urls() -> dict:
+
+
+    hospital_csv = os.path.normpath(os.path.join(abspath, 'hospitals.csv'))
+
+    # read in hospitals csv
+
+    df = pd.read_csv(hospital_csv)
+
+    print(df.head())
+
+    driver = create_driver(raw_download_path, driver_path)
+
+    hospital_data_urls = {}
+
+    unc_urls = []
+
+    ext = ['.json','.csv', 'wpfb_dl', '.xlsx', 'ptapp']
+
+    for index, row in df.iterrows():
+
+        records = []
+        
+        browser = get_url_data(row['hospital_url'], driver)
+
+        # get the page source and parse itS
+
+        source = browser.page_source
+
+        soup = BeautifulSoup(source, 'lxml')
+        
+        for entry in soup.find_all(['a'], href=True): 
+
+            download_url = entry.get('href')
+
+            if any(file_type in download_url for file_type in ext):
+                # determine if the base hospital_url is in download_url or not
+
+                if row['hospital_id'] in 'vidant-health' and '.csv' in download_url:
+                    continue
+
+                if download_url.startswith('/'):
+                    # if download_url starts with '/' then add the base hospital_url to the download_url
+
+                    data_url = urlparse(row['hospital_url']).scheme + "://" + urlparse(row['hospital_url']).netloc + '/'  + download_url
+
+                    records.append(data_url)
+
+                else:
+                    # if download_url does not start with '/' then add the base hospital_url to the download_url
+                    records.append(download_url)
+                
+        if 'first-health' in row['hospital_id']:
+
+            limit = 250
+
+            page = 0
+
+            first_health_url = row['hospital_url'].format(0)
+
+            output = requests.get(first_health_url).json()
+
+            output = requests.get(row['hospital_url'].format(0)).json()
+
+            page_limit_max = int((output['count'] / limit) + 1)
+
+            first_health_urls = [records.append(row['hospital_url'].format(page)) for page in range(1, page_limit_max)]
+            
+        if 'wakemed' in row['hospital_id']:
+
+            records.append(row['hospital_url'])
+            
+        hospital_data_urls[row['hospital_id']] = list(set(records))
+
+    return hospital_data_urls
 
 # get_unc('university-of-north-carolina-hospital')
 
-get_duke('duke-university-hospital')
+# define Prefect flow
+with Flow("hospital-price-transparency") as flow:
+    hospital_urls = get_source_urls()
+    # houston_realtor_data = transform(realtor_data)
+    # load_to_database = load(houston_realtor_data)
 
-get_north_carolina_baptist('north-carolina-baptist-hospital')
+flow.register(project_name="hospital-price-transparency")
 
-get_app('app-regional-health-system')
+flow.run_agent()
 
-get_catawba('catawba-valley-medical-center')
+flow.run()
+# get_duke('duke-university-hospital', hospital_urls)
 
-get_cateret('cateret-health-care')
+# get_north_carolina_baptist('north-carolina-baptist-hospital', hospital_urls)
 
-get_cone('cone-health')
+# get_app('app-regional-health-system', hospital_urls)
 
-get_first('first-health-moore')
+# get_catawba('catawba-valley-medical-center', hospital_urls)
 
-get_first('first-health-montgomery')
+# get_cateret('cateret-health-care', hospital_urls)
 
-get_iredell('iredell-health')
+# get_cone('cone-health', hospital_urls)
 
-get_mission('mission-health')
+# get_first('first-health-moore', hospital_urls)
 
-get_nhrmc('nhrmc-health')
+# get_first('first-health-montgomery', hospital_urls)
 
-get_northern('northern-regional')
+# get_iredell('iredell-health', hospital_urls)
 
-get_novant('novant-health')
+# get_mission('mission-health', hospital_urls)
 
-get_wakemed('wakemed-raleigh')
+# get_nhrmc('nhrmc-health', hospital_urls)
 
-get_wakemed('wakemed-cary')
+# get_northern('northern-regional', hospital_urls)
 
-get_vidant('vidant-health')
+# get_novant('novant-health', hospital_urls)
 
-get_atrium('atrium-health')
+# get_wakemed('wakemed-raleigh', hospital_urls)
+
+# get_wakemed('wakemed-cary', hospital_urls)
+
+# get_vidant('vidant-health', hospital_urls)
+
+# get_atrium('atrium-health', hospital_urls)
--- src/get_urls.py
+++ src/get_urls.py
@@ -5,100 +5,10 @@ from toolbox.scrapers import *
 
 # create hospital, download, and driver paths
 
-abspath = os.path.normpath(os.path.dirname(os.path.dirname(__file__)))
 
-hospitals = os.path.normpath(os.path.join(abspath, 'hospitals.csv'))
-
-raw_download_path = os.path.normpath(os.path.join(abspath, 'data', 'raw'))
-
-url_download_path = os.path.normpath(os.path.join(abspath, 'data', 'urls'))
-
-driver_path = os.path.join(abspath, 'drivers', 'chromedriver.exe')
-
-# read in hospitals csv
-
-df = pd.read_csv(hospitals)
 
 # loop through each hospital and ping standard charges page
 
-driver = create_driver(raw_download_path, driver_path)
 
-hospital_data_urls = {}
-
-unc_urls = []
-
-ext = ['.json','.csv', 'wpfb_dl', '.xlsx', 'ptapp']
-
-for index, row in df.iterrows():
-
-    records = []
-    
-    browser = get_url_data(driver, row['hospital_url'])
-
-    # get the page source and parse itS
-
-    source = browser.page_source
-
-    soup = BeautifulSoup(source, 'lxml')
-    
-    for entry in soup.find_all(['a'], href=True): 
-
-        download_url = entry.get('href')
-
-        if any(file_type in download_url for file_type in ext):
-            # determine if the base hospital_url is in download_url or not
-
-            if row['hospital_id'] in 'vidant-health' and '.csv' in download_url:
-                continue
-
-            if download_url.startswith('/'):
-                # if download_url starts with '/' then add the base hospital_url to the download_url
-
-                data_url = urlparse(row['hospital_url']).scheme + "://" + urlparse(row['hospital_url']).netloc + '/'  + download_url
-
-                records.append(data_url)
-
-            else:
-                # if download_url does not start with '/' then add the base hospital_url to the download_url
-                records.append(download_url)
-            
-    if 'first-health' in row['hospital_id']:
-
-        limit = 250
-
-        page = 0
-
-        first_health_url = row['hospital_url'].format(0)
-
-        output = requests.get(first_health_url).json()
-
-        output = requests.get(row['hospital_url'].format(0)).json()
-
-        page_limit_max = int((output['count'] / limit) + 1)
-
-        first_health_urls = [records.append(row['hospital_url'].format(page)) for page in range(1, page_limit_max)]
-        
-    if 'wakemed' in row['hospital_id']:
-
-        records.append(row['hospital_url'])
-        
-    hospital_data_urls[row['hospital_id']] = list(set(records))
-
-# remove hospital_data_urls keys where values are empty
-
-hospital_empty_urls = {k: v for k, v in hospital_data_urls.items() if v == []}
-
-hospital_contain_urls = {k: v for k, v in hospital_data_urls.items() if v != []}
-
-hospitals_collected = list(hospital_contain_urls.keys())
-
-hospital_not_collected = list(hospital_empty_urls.keys())
-
-# url_dictionary: {'hospital_id': [url1, url2, url3]} 
-
-
-# write to a json file
-
-open(os.path.join(url_download_path, 'hospital_data_urls.json'), 'w').write(json.dumps(hospital_data_urls))
 
 
