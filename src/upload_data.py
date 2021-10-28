fd67b7220477c3e67bcef36e9a4c55f97aff3f55
Will Clark <wfclark5@outlook.com>
2021-10-28 14:52:23 -0400

    Fixing contact-us

--- src/upload_data.py
+++ src/upload_data.py
@@ -12,7 +12,10 @@ profiles_path = os.path.normpath(os.path.join(abspath, 'docs', 'profiles'))
 
 make_zip(data_path, data_zip_path)
 
-# upload_to_s3('nc_health_cost', profiles_path, 'profiles')
+s3 = s3fs.core.S3FileSystem(profile="franz")
+
+
+upload_to_s3('nc_health_cost', profiles_path, 'profiles')
 
 upload_to_s3('nc_health_cost', data_zip_path, 'nc_health_cost_all.zip')
 
