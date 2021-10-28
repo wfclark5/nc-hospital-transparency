98a6fe5ae4d116266cdcf8342d014d3fd2d4e2e6
Will Clark <wfclark5@outlook.com>
2021-10-28 14:41:54 -0400

    Adding contact remove subscription and data upload

--- src/upload_data.py
+++ src/upload_data.py
@@ -1,5 +1,9 @@
 from process.profiles import *
 
+abspath = os.path.dirname(os.path.normpath(os.path.abspath(os.path.dirname(__file__))))
+
+profile_path = os.path.normpath(os.path.join(abspath, 'docs', 'profiles'))
+
 data_path = os.path.normpath(os.path.join(abspath, 'data'))
 
 data_zip_path = os.path.join(data_path, 'nc_health_cost_all.zip')
@@ -8,7 +12,7 @@ profiles_path = os.path.normpath(os.path.join(abspath, 'docs', 'profiles'))
 
 make_zip(data_path, data_zip_path)
 
-upload_to_s3('nc_health_cost', profiles_path, 'profiles')
+# upload_to_s3('nc_health_cost', profiles_path, 'profiles')
 
 upload_to_s3('nc_health_cost', data_zip_path, 'nc_health_cost_all.zip')
 
